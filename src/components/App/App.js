fdbf2ff0fea2a267442f0de67bda81f40b8aa4ef
Irina Abah <irin.bon@gmail.com>
2021-06-19 01:15:29 +0100

    feat: added basic jsx to aboutproject, me, techs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
@@ -18,11 +18,10 @@ function App() {
     <div className="App">
       <Header />
       <Switch>
-        {/* <Route exact path="/">
+        <Route exact path="/">
           {!loggedIn ? <Main /> : <Redirect to="/movies" />}
-          <Main />
         </Route>
-        <Route path="/movies">
+        {/* <Route path="/movies">
           <Movies />
         </Route>
         <Route path="/saved-movies">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import Navigation from '../Header/Navigation/Navigation';
 function Header() {
   return (
     <header className="header">
-      <Link to='/' className="header__link">
+      <Link to="/" className="header__link">
           <img className="header__logo" src={logoPath} alt="Movies portal logo" />
       </Link>
       <Route exact path="/">
