fb5eaedb5b397a2e32527cc6437e5bdbdaf4ad06
Irina Abah <irin.bon@gmail.com>
2021-06-30 00:45:08 +0300

    style: basic styles for portfolio

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -5,14 +5,12 @@ function AboutMe() {
     <section className="section about-me" id="about-me">
       <h2 className="section__title">Студент</h2>
       <div className="about-me__content">
-        <div className="about-me__info">
-          <h3 className="about-me__title">Ирина</h3>
-          <p className="about-me__subtitle">Фронтенд-разработчик, 32 года.</p>
-          <p className="about-me__bio">Я родилась, получила образование и первый опыт работы в Украине, сейчас живу в Великобритании. 5 лет до выхода в декрет работала в сфере ИТ на менеджерских позициях. В декрете поняла, что хочу развить давнее увлечение технической части разработки в полноценную профессию. С августа 2020 года изучаю веб-программирование с Яндекс.Практикум. Особый интерес в профессии для меня это сфера электронной коммерции (eCommerce).</p>
-          <div className="about-me__social">
-            <a className="about-me__social-item" href="http://facebook.com">Facebook</a>
-            <a className="about-me__social-item" href="http://github.com">Github</a>
-          </div>
+        <h3 className="about-me__title">Ирина</h3>
+        <p className="about-me__subtitle">Фронтенд-разработчик, 32 года.</p>
+        <p className="about-me__bio">Я родилась, получила образование и первый опыт работы в Украине, сейчас живу в Великобритании. 5 лет до выхода в декрет работала в сфере ИТ на менеджерских позициях. В декрете поняла, что хочу развить давнее увлечение технической части разработки в полноценную профессию. С августа 2020 года изучаю веб-программирование с Яндекс.Практикум. Особый интерес в профессии для меня это сфера электронной коммерции (eCommerce).</p>
+        <div className="about-me__social">
+          <a className="about-me__social-item" href="http://facebook.com">Facebook</a>
+          <a className="about-me__social-item" href="http://github.com">Github</a>
         </div>
         <img className="about-me__avatar" src={Avatar} alt="Photograph of Irina Abah" />
       </div>
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -7,11 +7,15 @@ function Portfolio() {
       <div className="portfolio__content">
         <div className="portfolio__item">
           <a className="portfolio__item-name" href="https://github.com/Irina-abah/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт
-          <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
           </a>
+          <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
+        </div>
+        <div className="portfolio__item">
           <a className="portfolio__item-name" href="https://irina-abah.github.io/russian-travel/index.html" target="_blank" rel="noreferrer">Адаптивный сайт
-          <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
           </a>
+          <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
+        </div>
+        <div className="portfolio__item">
           <a className="portfolio__item-name" href="https://github.com/Irina-abah/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение
           <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
           </a>
