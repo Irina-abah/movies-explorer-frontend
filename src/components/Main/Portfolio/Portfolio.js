37ff8bc9d2a5c58e3b8cd07e469f3ffb21ea785f
Irina Abah <irin.bon@gmail.com>
2021-06-30 12:48:44 +0300

    style: add Portfolio and Footer desktop styles

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,19 +6,23 @@ function Portfolio() {
       <h2 className="portfolio__title">Портфолио</h2>
       <div className="portfolio__content">
         <div className="portfolio__item">
-          <a className="portfolio__item-name" href="https://github.com/Irina-abah/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт
-          </a>
+          <p className="portfolio__item-name">Статичный сайт</p>
+          <a className="portfolio__link" href="https://github.com/Irina-abah/how-to-learn" target="_blank" rel="noreferrer">
           <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
+          </a>
         </div>
         <div className="portfolio__item">
-          <a className="portfolio__item-name" href="https://irina-abah.github.io/russian-travel/index.html" target="_blank" rel="noreferrer">Адаптивный сайт
-          </a>
+          <p className="portfolio__item-name">Адаптивный сайт</p>
+          <a className="portfolio__link" href="https://irina-abah.github.io/russian-travel/index.html" target="_blank" rel="noreferrer">
           <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
+          </a>
         </div>
         <div className="portfolio__item">
-          <a className="portfolio__item-name" href="https://github.com/Irina-abah/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение
-          <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
+          <p className="portfolio__item-name">Одностраничное приложение</p>
+          <a className="portfolio__link" href="https://github.com/Irina-abah/react-mesto-api-full" target="_blank" rel="noreferrer">
+            <img className="portfolio__icon" src={Arrow} alt="Arrow icon" />
           </a>
+          
         </div>
       </div>
     </section>
