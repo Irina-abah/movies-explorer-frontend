6fc909cb1e753bebf666295e272904aafdd73d72
Irina Abah <irin.bon@gmail.com>
2021-07-15 00:12:50 +0300

    feat: added trailer to MovieCard

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -9,9 +9,13 @@ function MoviesCard(props) {
     setIsSaved(!isSaved);
   }
 
-  const duration = `${Math.floor(props.movie.duration / 60)}ч ${props.movie.duration % 60}м`;
+  function openTrailer() {
+    window.open(props.movie.trailerLink);
+}
 
-  const movieCardClassName = `button button_type_save ${isSaved ? 'button_type_save-active' : ''}`; 
+  const duration = `${Math.floor(props.movie.duration / 60)}ч ${props.movie.duration % 60}м`;
+  const movieCardClassName = `button button_type_save ${isSaved ? 'button_type_save-active' : ''}`;
+  
 
   return (
     <li className="movie">
@@ -23,17 +27,17 @@ function MoviesCard(props) {
           </div>
           <button className={movieCardClassName} type="button" aria-label="delete button" onClick={handleSaveClick}/>
         </div>
-        <img className="movie__image" src={'https://api.nomoreparties.co' + props.movie.image.formats.thumbnail.url} alt={props.movie.nameRU}/>
+        <img className="movie__image" src={'https://api.nomoreparties.co' + props.movie.image.formats.thumbnail.url} alt={props.movie.nameRU} onClick={openTrailer}/>
       </Route>
       <Route path="/saved-movies">
         <div className="movie__info">
           <div className="movie__description">
             <h2 className="movie__title">{props.movie.nameRU}</h2>
-            <p className="movie__duration">{props.movie.duration}</p>
+            <p className="movie__duration">{duration}</p>
           </div>
           <button className="button button_type_delete" type="button" aria-label="delete button"/>
         </div>
-        <img className="movie__image" src={props.movie.image} alt={props.movie.nameRU}/>
+        <img className="movie__image" src={'https://api.nomoreparties.co' + props.movie.image.formats.thumbnail.url} alt={props.movie.nameRU} onClick={openTrailer}/>
       </Route>
     </li>
   )
